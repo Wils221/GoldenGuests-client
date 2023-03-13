3cbe830e03c9b3d1c9f5ecfe50ad40db47626b0d
Wil Hammond <wil.hammond@gmail.com>
2023-03-13 15:43:24 -0500

    src

---
+++ src/managers/EventManager.js
@@ -0,0 +1,70 @@
+export const getEvents = () => {
+    return fetch("http://localhost:8000/events", {
+        headers:{
+            "Authorization": `Token ${localStorage.getItem("lu_token")}`
+        }
+    })
+        .then(response => response.json())
+}
+export const createEvent = (event) => {
+    return fetch("http://localhost:8000/events", {
+        method: "POST", 
+        headers:{
+            "Authorization":`Token ${localStorage.getItem("lu_token")}`,
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(event)
+    })
+}
+
+export const UpdateEvent = (event, eventId) => {
+    return fetch(`http://localhost:8000/events/${eventId}`, {
+        method: "PUT", 
+        headers:{
+            "Authorization":`Token ${localStorage.getItem("lu_token")}`,
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(event)
+    })
+}
+
+export const getSingleEvent = (eventId) => {
+    return fetch(`http://localhost:8000/events/${eventId}`, {
+        method: "GET",
+        headers:{
+            "Authorization": `Token ${localStorage.getItem("lu_token")}`
+        }
+    })
+        .then(response => response.json())
+}
+
+export const deleteEvent = (eventId) => {
+    return fetch(`http://localhost:8000/events/${eventId}`, {
+        method: "DELETE",
+        headers:{
+            "Authorization": `Token ${localStorage.getItem("lu_token")}`
+        }
+    })
+}
+export const leaveEvent = (id) => {
+    return fetch(`http://localhost:8000/events/${id}/leave`, 
+    {
+        method: "DELETE",
+        headers:{
+            "Authorization": `Token ${localStorage.getItem("lu_token")}`
+        }
+    })  
+}
+
+export const joinEvent = (id) => {
+    return fetch(`http://localhost:8000/events/${id}/signup`, {
+        method: "POST",
+        headers: {
+        "Authorization": `Token ${localStorage.getItem("lu_token")}`,
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+        },
+        body: JSON.stringify(id)
+    })
+        .then(res => res.json())
+}
---
+++ src/managers/GameManager.js
@@ -0,0 +1,59 @@
+
+export const getGames = () => {
+    return fetch("http://localhost:8000/games", {
+        headers:{
+            "Authorization": `Token ${localStorage.getItem("lu_token")}`
+        }
+    })
+        .then(response => response.json())
+}
+export const createGame = (game) => {
+    return fetch("http://localhost:8000/games", {
+        method: "POST", 
+        headers:{
+            "Authorization":`Token ${localStorage.getItem("lu_token")}`,
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(game)
+    })
+}
+
+export const getGenres = () => {
+    return fetch("http://localhost:8000/genres", { 
+        headers:{
+            "Authorization":`Token ${localStorage.getItem("lu_token")}`
+        }
+    })
+        .then((response => response.json()))
+}
+
+export const UpdateGame = (game, gameId) => {
+    return fetch(`http://localhost:8000/games/${gameId}`, {
+        method: "PUT", 
+        headers:{
+            "Authorization":`Token ${localStorage.getItem("lu_token")}`,
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(game)
+    })
+}
+
+export const getSingleGame = (gameId) => {
+    return fetch(`http://localhost:8000/games/${gameId}`, {
+        method: "GET",
+        headers:{
+            "Authorization": `Token ${localStorage.getItem("lu_token")}`
+        }
+    })
+        .then(response => response.json())
+}
+
+export const deleteGame = (id) => {
+    return fetch(`http://localhost:8000/games/${id}`, 
+    {
+        method: "DELETE",
+        headers:{
+            "Authorization": `Token ${localStorage.getItem("lu_token")}`
+        }
+    })
+}
